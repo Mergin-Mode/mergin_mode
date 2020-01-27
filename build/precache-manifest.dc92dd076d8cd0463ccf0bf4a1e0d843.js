bda198601a56edc30b17573445808bc6980456dd
Dr <priestont@gmail.com>
2020-01-27 21:58:41 +0200

    prgs

---
+++ build/precache-manifest.dc92dd076d8cd0463ccf0bf4a1e0d843.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfe482d403842e3ff764db09ec65943",
+    "url": "/mergin_mode/index.html"
+  },
+  {
+    "revision": "8824f9658dac22719a56",
+    "url": "/mergin_mode/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "1d1f95252d3974cc35ee",
+    "url": "/mergin_mode/static/css/main.25875f71.chunk.css"
+  },
+  {
+    "revision": "8824f9658dac22719a56",
+    "url": "/mergin_mode/static/js/2.80ab89a5.chunk.js"
+  },
+  {
+    "revision": "801eb6b718cfeb49e4699ab80a85e8cf",
+    "url": "/mergin_mode/static/js/2.80ab89a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d1f95252d3974cc35ee",
+    "url": "/mergin_mode/static/js/main.9c458b5a.chunk.js"
+  },
+  {
+    "revision": "59f104aae7ff6a2e5f21",
+    "url": "/mergin_mode/static/js/runtime-main.69861f9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mergin_mode/static/media/logo.5d5d9eef.svg"
+  }
+]);
